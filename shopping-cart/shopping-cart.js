e126884794f231ea73e4ece2ecad0571c5c19707
Kai Weluda <kaiweluda@gmail.com>
2023-04-21 13:29:02 +0200

    feat: adds 'Shopping Cart' kata

--- shopping-cart/shopping-cart.js
+++ shopping-cart/shopping-cart.js
@@ -1,3 +1,42 @@
 class ShoppingCart {
-  // todo: implement your code here
+  cart = [];
+
+  addToCart(description, price, quantity) {
+    this.cart.push({
+      description: description,
+      price: price,
+      quantity: quantity,
+    });
+  }
+
+  getCartPrice() {
+    let sum = 0;
+    this.cart.forEach((element) => {
+      sum += element.price * element.quantity;
+    });
+    return sum;
+  }
+
+  removeFromCart(description, quantity) {
+    if (arguments.length === 1) {
+      this.cart.forEach((element, index) => {
+        if (element.description === description) {
+          this.cart.splice(index, 1);
+        }
+      });
+    } else {
+      this.cart.forEach((element) => {
+        if (element.description === description) {
+          element.quantity -= quantity;
+        }
+      });
+    }
+  }
 }
+
+const exampleCart = new ShoppingCart();
+exampleCart.addToCart("Kirschen", 2, 4);
+console.log(exampleCart.cart);
+console.log(typeof exampleCart.cart);
+console.log(exampleCart.removeFromCart("Kirschen", 3));
+console.log(exampleCart.cart);
