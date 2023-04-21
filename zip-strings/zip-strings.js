66e23c8c579e5fa54569e040e17d122af790a14f
Kai Weluda <kaiweluda@gmail.com>
2023-04-21 10:26:29 +0200

    feat: adds 'Zip String' kata

--- zip-strings/zip-strings.js
+++ zip-strings/zip-strings.js
@@ -1,4 +1,20 @@
 function zipStrings(strA, strB) {
-  // Add you solution here
-  return "a1b2c3";
+  let outputArray = [];
+  let strArrayA = strA.split("");
+  let strArrayB = strB.split("");
+  let finalIndex = strArrayA.length + strArrayB.length;
+
+  for (let i = 0; i < finalIndex; i++) {
+    if (i % 2 === 0 && strArrayA.length !== 0) {
+      outputArray.push(strArrayA.shift());
+    } else if (i % 2 !== 0 && strArrayB.length !== 0) {
+      outputArray.push(strArrayB.shift());
+    } else if (strArrayA.length === 0) {
+      outputArray.push(strArrayB.shift());
+    } else {
+      outputArray.push(strArrayA.shift());
+    }
+  }
+  return outputArray.join("");
 }
+console.log(zipStrings("abc", "123"));
