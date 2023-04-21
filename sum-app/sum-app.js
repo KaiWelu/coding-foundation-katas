03b9f44e10ccd9fa59ae82b94ab4a384247175f5
Kai Weluda <kaiweluda@gmail.com>
2023-04-21 09:21:49 +0200

    feat: adds 'Sum App' kata

--- sum-app/sum-app.js
+++ sum-app/sum-app.js
@@ -1,3 +1,28 @@
 class SumApp {
-  // Add you solution here
+  numbers = [];
+
+  addNumber(n) {
+    this.numbers.push(n);
+  }
+
+  getSum() {
+    if (this.numbers.length === 0) {
+      return 0;
+    }
+    let count = 0;
+    this.numbers.forEach((element) => {
+      count += element;
+    });
+    return count;
+  }
+
+  reset() {
+    this.numbers = [];
+  }
 }
+
+const sum = new SumApp();
+
+sum.addNumber(5);
+sum.addNumber(6);
+console.log(sum.getSum());
